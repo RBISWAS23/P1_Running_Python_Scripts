54c5abf2901fb428399eb7a61d4e23685ae04d43
RBISWAS <rahimbws452@gmail.com>
2023-08-29 14:10:18 +0530

    Updated 29/8/23

---
+++ arithmetic_operations.py
@@ -0,0 +1,18 @@
+num_1 = 12
+num_2 = 63
+
+# Addition
+addition = num_1 + num_2
+print("The addition of {} and {} is {}".format(num_1, num_2, addition))
+
+# Subtraction
+subtraction = num_1 - num_2
+print("The subtraction of {} and {} is {}".format(num_1, num_2, subtraction))
+
+# Division
+div = num_1 / num_2
+print("The division of {} by {} is {}".format(num_1, num_2, div))
+
+# Multiplication
+mult = num_1 * num_2
+print("The multiplication of {} and {} is {}".format(num_1, num_2, mult))
